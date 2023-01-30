725da9e81aded2a04e330b5c90bee93e6dae0ee3
SantinoMamani <santinomamani25@gmail.com>
2023-01-30 13:16:22 -0300

    trabajos realizados

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,14 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
-}
+    //	alert("ok");
 
+    // var edad;
+    let nombre;
+	
+	// nombre = "santino"; // Asignacion de string
+	nombre = prompt("ingrese un nombre");
+
+	// mostrar por alert
+	alert(nombre)
+}
